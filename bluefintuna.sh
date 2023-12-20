45c816829d4f76967449c748f00973b5981fe93d
Goldenmonkeyy <72548001+Goldenmonkeyy@users.noreply.github.com>
2023-12-20 10:23:15 +0800

    Update bluefintuna.sh

--- bluefintuna.sh
+++ bluefintuna.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+red='\033[0;31m'
+green='\033[0;32m'
+clear='\033[0m'
 
 #Get input values
 read -p "Please input backend language, java or php: " Lang
@@ -14,12 +17,12 @@ echo "-------------------------------------------------------------------------"
 read -p "What's the middleware, tomcat, weblogic or ngnix: " Midware
 echo "The middware is $Midware."
 echo "-------------------------------------------------------------------------"
-
+echo -e "\n"
 
 
 #is the user root?
 if [ $(whoami) == root ]; then 
-        echo "Sorry, must run under root!"
+        echo -e "${red}Sorry, must run under root!${clear}"
         exit 0
 fi
 
@@ -27,32 +30,35 @@ fi
 
 #ip address
 iplocal=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-echo "The local ip address is: $iplocal"
+echo -e "${green}The local ip address is: $iplocal${clear}"
+echo -e "\n"
 
 #system date
 datenow=$(date +"%Y-%m-%d")
-echo "System date is: $datenow"
+echo -e "${green}System date is: $datenow${clear}"
+echo -e "\n"
 
 #1. webshell lookup module:
 
-
 #find possible webapps folder
-echo "Possible webapps root folders are: "
-timeout 20 bash -c 'find / -type d \( -name "*tomcat" -o -name "*weblogic*"\) 2>/dev/null'
+echo -e "${green}Possible webapps root folders are: ${clear}"
+timeout 20 bash -c 'find / -type d \( -iname "*tomcat*" -o -iname "*weblogic*" \) 2>/dev/null'
+echo -e "\n"
 
 #start of finding webshell files
 if [ -z "$ShellName" ]
 then
-      echo "Possible webshells are:"
-      timeout 40 bash -c 'find / -type f -name "*.jsp" 2>/dev/null'
+      echo -e "${green}Possible webshells are:${clear}"
+      timeout 40 bash -c 'find / -type f -iname "*.jsp" 2>/dev/null'
 else
-      echo "the path of shellname are: "
-      timeout 40 bash -c 'find / -type f -name "$ShellName" 2>/dev/null'
+        echo -e "${green}the path of shellname are: ${clear}"
+      timeout 40 bash -c 'find / -type f -iname "$ShellName" 2>/dev/null'
 fi
 
 #2. suspect file lookupp
-echo "The following files look suspicious: "
-timeout 40 bash -c 'find / -type f \(-name "*portmap*"-o -name "tunnel.jsp" -o -name "suo5.jsp" -o -name "*fscan*" -o -name "*unicornscan*" -o -name "conn.jsp"\) 2>/dev/null'
+echo -e "\n"
+echo -e "${green}The following files look suspicious: ${clear}"
+timeout 40 bash -c 'find / -type f \( -iname "*portmap*"-o -iname "tunnel.jsp" -o -iname "suo5.jsp" -o -iname "*fscan*" -o -iname "*unicornscan*" -o -iname "conn.jsp" \) 2>/dev/null'
 
 
-#3.
+#3. log file analysis 
